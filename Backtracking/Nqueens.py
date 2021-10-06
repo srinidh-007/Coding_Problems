f762242f24ff3453fe3f56659af507b230525303
SURYACODER <60681084+SURYACODER@users.noreply.github.com>
2021-10-06 14:45:13 +0530

    Updated Nqueens.py
    Added the comments part to it.

--- Backtracking/Nqueens.py
+++ Backtracking/Nqueens.py
@@ -1,38 +1,42 @@
-import numpy as np
+#To impelement N-Queens problems in python
+import numpy as np      
 def print_table(board):
     l = len(board)
-    print(' '*l+"solution"+' '*l)
-    print('----'*l)
-    for i in board:
-        print('',end="")
+    print(' '*l+"solution"+' '*l)       #giving spaces based on the length of board
+    print('----'*l)                         
+    for i in board:                                          
+        print('',end="")                
         for j in i :
             if j==1:
-                print('| Q ',end='')
+                print('| Q ',end='')        #printing the queen on the imaginary chess board for a possible solution
             else :
-                print('|   ',end='')
+                print('|   ',end='')        #otherwise just print the space with this symbol '|'
         print('|',end="")
-        print('\n'+'----'*l)
+        print('\n'+'----'*l)                #take the next line and apply the same loop as above 
+        
+    # This function is to check the placing of queens on the chess board so that none of them are in attacking positions 
+    # This calls the print_table function so that the board is printed accordingly
+    
 def placing(n,board,row,col):
   if(row==n):
-        #print(board)
         print_table(board)
   else:
     for i in range(n) :
-        if(check(n,board,row,i)==True):
+        if(check(n,board,row,i)==True):         #if the conditions satisfy then we place the queens accordingly
             board[row][i] = 1
             placing(n,board,row+1,i)
             board[row][i] = 0
-def check(n,board,row,col):
+def check(n,board,row,col):         #this function checks the conditions such that queens are not placed in attacking positions including the diagonality check
     i,j,c = row,col,0
     while(i>=0 and j>=0):
-        if(board[i][j]==1):
+        if(board[i][j]==1):         #here i,j,c is taken to keep track of no of rows, no of columns, no of queens that are checked with conditions respectively
             c += 1
         i -= 1
         j -= 1
-    if(c>0): return False
+    if(c>0): return False           
     i,j,c = row,col,0
     while(i<n and j<n):
-        if(board[i][j]==1):
+        if(board[i][j]==1):         
             c += 1
         i += 1
         j += 1
@@ -64,7 +68,7 @@ def check(n,board,row,col):
         j += 1
     if(c>0): return False
     return True
-n = int(input("Enter no of Queens : "))
-board = np.zeros((n,n),dtype=int)
+n = int(input("Enter no of Queens : "))         #taking the input for number of queens 
+board = np.zeros((n,n),dtype=int)                   #intially putting zeroes on board array
 print("The possible solutions : ")
 placing(n,board,0,0)
